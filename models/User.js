569d1ed95124879e20a1ec97b81c2c2d81704103
codepanther <pezechigozirim@gmail.com>
2022-07-19 03:05:37 +0100

    fix typo in User model

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const UserSchema = new mongoose.Schema(
     },
     email: {
       type: String,
-      require: [true, 'email is required'],
+      required: [true, 'email is required'],
       unique: true
     },
     password: {
